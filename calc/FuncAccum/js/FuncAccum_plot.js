edea0385acfe29207f5a42134fdc9a3f8240f4bd
Thomas J Pfaff <tpfaff99@outlook.com>
2026-07-14 12:25:21 -0400

    Add files via upload

---
+++ calc/FuncAccum/js/FuncAccum_plot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#FuncAccum_plot').scianimator({
+	    'images': ['FuncAccum_dir/FuncAccum_plot1.png', 'FuncAccum_dir/FuncAccum_plot2.png', 'FuncAccum_dir/FuncAccum_plot3.png', 'FuncAccum_dir/FuncAccum_plot4.png', 'FuncAccum_dir/FuncAccum_plot5.png', 'FuncAccum_dir/FuncAccum_plot6.png', 'FuncAccum_dir/FuncAccum_plot7.png', 'FuncAccum_dir/FuncAccum_plot8.png', 'FuncAccum_dir/FuncAccum_plot9.png', 'FuncAccum_dir/FuncAccum_plot10.png', 'FuncAccum_dir/FuncAccum_plot11.png', 'FuncAccum_dir/FuncAccum_plot12.png', 'FuncAccum_dir/FuncAccum_plot13.png', 'FuncAccum_dir/FuncAccum_plot14.png', 'FuncAccum_dir/FuncAccum_plot15.png', 'FuncAccum_dir/FuncAccum_plot16.png', 'FuncAccum_dir/FuncAccum_plot17.png', 'FuncAccum_dir/FuncAccum_plot18.png', 'FuncAccum_dir/FuncAccum_plot19.png', 'FuncAccum_dir/FuncAccum_plot20.png', 'FuncAccum_dir/FuncAccum_plot21.png', 'FuncAccum_dir/FuncAccum_plot22.png', 'FuncAccum_dir/FuncAccum_plot23.png', 'FuncAccum_dir/FuncAccum_plot24.png', 'FuncAccum_dir/FuncAccum_plot25.png', 'FuncAccum_dir/FuncAccum_plot26.png', 'FuncAccum_dir/FuncAccum_plot27.png', 'FuncAccum_dir/FuncAccum_plot28.png', 'FuncAccum_dir/FuncAccum_plot29.png', 'FuncAccum_dir/FuncAccum_plot30.png', 'FuncAccum_dir/FuncAccum_plot31.png', 'FuncAccum_dir/FuncAccum_plot32.png', 'FuncAccum_dir/FuncAccum_plot33.png', 'FuncAccum_dir/FuncAccum_plot34.png', 'FuncAccum_dir/FuncAccum_plot35.png', 'FuncAccum_dir/FuncAccum_plot36.png', 'FuncAccum_dir/FuncAccum_plot37.png', 'FuncAccum_dir/FuncAccum_plot38.png', 'FuncAccum_dir/FuncAccum_plot39.png', 'FuncAccum_dir/FuncAccum_plot40.png', 'FuncAccum_dir/FuncAccum_plot41.png', 'FuncAccum_dir/FuncAccum_plot42.png', 'FuncAccum_dir/FuncAccum_plot43.png', 'FuncAccum_dir/FuncAccum_plot44.png', 'FuncAccum_dir/FuncAccum_plot45.png', 'FuncAccum_dir/FuncAccum_plot46.png', 'FuncAccum_dir/FuncAccum_plot47.png', 'FuncAccum_dir/FuncAccum_plot48.png', 'FuncAccum_dir/FuncAccum_plot49.png', 'FuncAccum_dir/FuncAccum_plot50.png', 'FuncAccum_dir/FuncAccum_plot51.png'],
+	    'width': 800,
+	    'delay': 200,
+	    'loopMode': 'loop',
+ 'controls': ['first', 'previous', 'play', 'next', 'last', 'loop', 'speed']
+	});
+    });
+})(jQuery);
