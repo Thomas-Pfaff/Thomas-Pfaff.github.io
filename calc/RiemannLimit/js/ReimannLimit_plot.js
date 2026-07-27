855a0eb4775720e7676320ce32f4e99b16942c4a
Thomas J Pfaff <tpfaff99@outlook.com>
2026-07-27 14:06:05 -0400

    Add files via upload

---
+++ calc/RiemannLimit/js/ReimannLimit_plot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#ReimannLimit_plot').scianimator({
+	    'images': ['RiemannLimit/ReimannLimit_plot1.png', 'RiemannLimit/ReimannLimit_plot2.png', 'RiemannLimit/ReimannLimit_plot3.png', 'RiemannLimit/ReimannLimit_plot4.png', 'RiemannLimit/ReimannLimit_plot5.png', 'RiemannLimit/ReimannLimit_plot6.png', 'RiemannLimit/ReimannLimit_plot7.png', 'RiemannLimit/ReimannLimit_plot8.png', 'RiemannLimit/ReimannLimit_plot9.png', 'RiemannLimit/ReimannLimit_plot10.png', 'RiemannLimit/ReimannLimit_plot11.png', 'RiemannLimit/ReimannLimit_plot12.png', 'RiemannLimit/ReimannLimit_plot13.png', 'RiemannLimit/ReimannLimit_plot14.png', 'RiemannLimit/ReimannLimit_plot15.png', 'RiemannLimit/ReimannLimit_plot16.png', 'RiemannLimit/ReimannLimit_plot17.png', 'RiemannLimit/ReimannLimit_plot18.png', 'RiemannLimit/ReimannLimit_plot19.png', 'RiemannLimit/ReimannLimit_plot20.png', 'RiemannLimit/ReimannLimit_plot21.png'],
+	    'width': 800,
+	    'delay': 1000,
+	    'loopMode': 'loop',
+ 'controls': ['first', 'previous', 'play', 'next', 'last', 'loop', 'speed']
+	});
+    });
+})(jQuery);
