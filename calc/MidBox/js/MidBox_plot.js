df9466379f04b14dd9fd0d8df92ce2305d652ccf
Thomas J Pfaff <tpfaff99@outlook.com>
2026-07-27 14:03:25 -0400

    Add files via upload

---
+++ calc/MidBox/js/MidBox_plot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#MidBox_plot').scianimator({
+	    'images': ['MidBox_dir/MidBox_plot1.png', 'MidBox_dir/MidBox_plot2.png', 'MidBox_dir/MidBox_plot3.png', 'MidBox_dir/MidBox_plot4.png', 'MidBox_dir/MidBox_plot5.png', 'MidBox_dir/MidBox_plot6.png', 'MidBox_dir/MidBox_plot7.png', 'MidBox_dir/MidBox_plot8.png', 'MidBox_dir/MidBox_plot9.png', 'MidBox_dir/MidBox_plot10.png', 'MidBox_dir/MidBox_plot11.png', 'MidBox_dir/MidBox_plot12.png', 'MidBox_dir/MidBox_plot13.png', 'MidBox_dir/MidBox_plot14.png', 'MidBox_dir/MidBox_plot15.png', 'MidBox_dir/MidBox_plot16.png', 'MidBox_dir/MidBox_plot17.png', 'MidBox_dir/MidBox_plot18.png', 'MidBox_dir/MidBox_plot19.png', 'MidBox_dir/MidBox_plot20.png', 'MidBox_dir/MidBox_plot21.png', 'MidBox_dir/MidBox_plot22.png', 'MidBox_dir/MidBox_plot23.png', 'MidBox_dir/MidBox_plot24.png', 'MidBox_dir/MidBox_plot25.png', 'MidBox_dir/MidBox_plot26.png', 'MidBox_dir/MidBox_plot27.png', 'MidBox_dir/MidBox_plot28.png', 'MidBox_dir/MidBox_plot29.png', 'MidBox_dir/MidBox_plot30.png', 'MidBox_dir/MidBox_plot31.png', 'MidBox_dir/MidBox_plot32.png'],
+	    'width': 800,
+	    'delay': 1000,
+	    'loopMode': 'loop',
+ 'controls': ['first', 'previous', 'play', 'next', 'last', 'loop', 'speed']
+	});
+    });
+})(jQuery);
