6576067a245a18ea01df0c8ae360b9c572680d83
Thomas J Pfaff <tpfaff99@outlook.com>
2026-07-27 14:04:50 -0400

    Add files via upload

---
+++ calc/RightBox/js/RightBox_plot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#RightBox_plot').scianimator({
+	    'images': ['RightBox_dir/RightBox_plot1.png', 'RightBox_dir/RightBox_plot2.png', 'RightBox_dir/RightBox_plot3.png', 'RightBox_dir/RightBox_plot4.png', 'RightBox_dir/RightBox_plot5.png', 'RightBox_dir/RightBox_plot6.png', 'RightBox_dir/RightBox_plot7.png', 'RightBox_dir/RightBox_plot8.png', 'RightBox_dir/RightBox_plot9.png', 'RightBox_dir/RightBox_plot10.png', 'RightBox_dir/RightBox_plot11.png', 'RightBox_dir/RightBox_plot12.png', 'RightBox_dir/RightBox_plot13.png', 'RightBox_dir/RightBox_plot14.png', 'RightBox_dir/RightBox_plot15.png', 'RightBox_dir/RightBox_plot16.png', 'RightBox_dir/RightBox_plot17.png', 'RightBox_dir/RightBox_plot18.png', 'RightBox_dir/RightBox_plot19.png', 'RightBox_dir/RightBox_plot20.png', 'RightBox_dir/RightBox_plot21.png', 'RightBox_dir/RightBox_plot22.png', 'RightBox_dir/RightBox_plot23.png', 'RightBox_dir/RightBox_plot24.png', 'RightBox_dir/RightBox_plot25.png', 'RightBox_dir/RightBox_plot26.png', 'RightBox_dir/RightBox_plot27.png', 'RightBox_dir/RightBox_plot28.png', 'RightBox_dir/RightBox_plot29.png', 'RightBox_dir/RightBox_plot30.png', 'RightBox_dir/RightBox_plot31.png', 'RightBox_dir/RightBox_plot32.png'],
+	    'width': 800,
+	    'delay': 1000,
+	    'loopMode': 'loop',
+ 'controls': ['first', 'previous', 'play', 'next', 'last', 'loop', 'speed']
+	});
+    });
+})(jQuery);
