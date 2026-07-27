fc136b274aa270b767820a9df18397fe5f54c8e0
Thomas J Pfaff <tpfaff99@outlook.com>
2026-07-27 14:00:39 -0400

    Add files via upload

---
+++ calc/LeftBox/js/LeftBox_plot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#LeftBox_plot').scianimator({
+	    'images': ['LeftBox_dir/LeftBox_plot1.png', 'LeftBox_dir/LeftBox_plot2.png', 'LeftBox_dir/LeftBox_plot3.png', 'LeftBox_dir/LeftBox_plot4.png', 'LeftBox_dir/LeftBox_plot5.png', 'LeftBox_dir/LeftBox_plot6.png', 'LeftBox_dir/LeftBox_plot7.png', 'LeftBox_dir/LeftBox_plot8.png', 'LeftBox_dir/LeftBox_plot9.png', 'LeftBox_dir/LeftBox_plot10.png', 'LeftBox_dir/LeftBox_plot11.png', 'LeftBox_dir/LeftBox_plot12.png', 'LeftBox_dir/LeftBox_plot13.png', 'LeftBox_dir/LeftBox_plot14.png', 'LeftBox_dir/LeftBox_plot15.png', 'LeftBox_dir/LeftBox_plot16.png', 'LeftBox_dir/LeftBox_plot17.png', 'LeftBox_dir/LeftBox_plot18.png', 'LeftBox_dir/LeftBox_plot19.png', 'LeftBox_dir/LeftBox_plot20.png', 'LeftBox_dir/LeftBox_plot21.png', 'LeftBox_dir/LeftBox_plot22.png', 'LeftBox_dir/LeftBox_plot23.png', 'LeftBox_dir/LeftBox_plot24.png', 'LeftBox_dir/LeftBox_plot25.png', 'LeftBox_dir/LeftBox_plot26.png', 'LeftBox_dir/LeftBox_plot27.png', 'LeftBox_dir/LeftBox_plot28.png', 'LeftBox_dir/LeftBox_plot29.png', 'LeftBox_dir/LeftBox_plot30.png', 'LeftBox_dir/LeftBox_plot31.png', 'LeftBox_dir/LeftBox_plot32.png'],
+	    'width': 800,
+	    'delay': 1000,
+	    'loopMode': 'loop',
+ 'controls': ['first', 'previous', 'play', 'next', 'last', 'loop', 'speed']
+	});
+    });
+})(jQuery);
