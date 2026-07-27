b4c34f6bb05570cbe2643a5145a9b49d0b2f5951
Thomas J Pfaff <tpfaff99@outlook.com>
2026-07-27 14:05:31 -0400

    Add files via upload

---
+++ calc/RightBox2/js/RightBox2_plot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#RightBox2_plot').scianimator({
+	    'images': ['RightBox2_dir/RightBox2_plot1.png', 'RightBox2_dir/RightBox2_plot2.png', 'RightBox2_dir/RightBox2_plot3.png', 'RightBox2_dir/RightBox2_plot4.png', 'RightBox2_dir/RightBox2_plot5.png', 'RightBox2_dir/RightBox2_plot6.png', 'RightBox2_dir/RightBox2_plot7.png', 'RightBox2_dir/RightBox2_plot8.png', 'RightBox2_dir/RightBox2_plot9.png', 'RightBox2_dir/RightBox2_plot10.png', 'RightBox2_dir/RightBox2_plot11.png', 'RightBox2_dir/RightBox2_plot12.png', 'RightBox2_dir/RightBox2_plot13.png', 'RightBox2_dir/RightBox2_plot14.png', 'RightBox2_dir/RightBox2_plot15.png', 'RightBox2_dir/RightBox2_plot16.png', 'RightBox2_dir/RightBox2_plot17.png', 'RightBox2_dir/RightBox2_plot18.png', 'RightBox2_dir/RightBox2_plot19.png', 'RightBox2_dir/RightBox2_plot20.png', 'RightBox2_dir/RightBox2_plot21.png', 'RightBox2_dir/RightBox2_plot22.png', 'RightBox2_dir/RightBox2_plot23.png', 'RightBox2_dir/RightBox2_plot24.png', 'RightBox2_dir/RightBox2_plot25.png', 'RightBox2_dir/RightBox2_plot26.png', 'RightBox2_dir/RightBox2_plot27.png', 'RightBox2_dir/RightBox2_plot28.png', 'RightBox2_dir/RightBox2_plot29.png', 'RightBox2_dir/RightBox2_plot30.png', 'RightBox2_dir/RightBox2_plot31.png', 'RightBox2_dir/RightBox2_plot32.png', 'RightBox2_dir/RightBox2_plot33.png', 'RightBox2_dir/RightBox2_plot34.png', 'RightBox2_dir/RightBox2_plot35.png', 'RightBox2_dir/RightBox2_plot36.png'],
+	    'width': 800,
+	    'delay': 1000,
+	    'loopMode': 'loop',
+ 'controls': ['first', 'previous', 'play', 'next', 'last', 'loop', 'speed']
+	});
+    });
+})(jQuery);
